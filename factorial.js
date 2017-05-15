c554b9671b36daab1be69e93f717eed205f5b42c
Frank Iriarte <jurrish@gmail.com>
2017-05-15 11:23:02 -0700

    logs

--- factorial.js
+++ factorial.js
@@ -9,3 +9,8 @@ let factorial = function(n){
 factorial(3); // 3 * 2 * 1
 factorial(4); // 4 * 3 * 2 * 1
 factorial(5); // 5 * 4 * 3 * 2 * 1
+
+
+console.log('factorial(3) => ', factorial(3));
+console.log('factorial(4) => ', factorial(4));
+console.log('factorial(5) => ', factorial(5));
