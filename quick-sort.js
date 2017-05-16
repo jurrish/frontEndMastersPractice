67227e76ae25c26e4009ad2cc41471264e22419a
Frank Iriarte <jurrish@gmail.com>
2017-05-16 14:07:55 -0700

    quick-sort should be working but there is something wrong

---
+++ quick-sort.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let nums = [1,5,10,3, 7, 7, 9, 2];
+
+const quickSort = (nums) => {
+  if(nums.length <= 1) return nums;
+  let pivot = nums[nums.length - 1];
+  let left = []; //values smaller than pivot
+  let right = []; //values greater than pivot
+  //account for pivot in the array length (last index => length -1)
+  for(let i = 0; i < nums.length-1; i++){
+
+    if(nums[i] < pivot){
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+  //or
+  return quickSort(left).concat(pivot, quickSort(right));
+  // return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+quickSort(nums);
