9d12a77ec991dcb7f9d5db9fc088c0f4c3a80de3
Frank Iriarte <jurrish@gmail.com>
2017-05-18 17:44:40 -0700

    still practicing quickSort

--- quick-sort.js
+++ quick-sort.js
@@ -1,26 +1,44 @@
 'use strict';
 
-let nums = [1,5,10,3, 7, 7, 9, 2];
+// let nums = [1,5,10,3, 7, 7, 9, 2];
+//
+// const quickSort = (nums) => {
+//   if(nums.length <= 1) return nums;
+//   let pivot = nums[nums.length - 1];
+//   let left = []; //values smaller than pivot
+//   let right = []; //values greater than pivot
+//   //account for pivot in the array length (last index => length -1)
+//   for(let i = 0; i < nums.length-1; i++){
+//
+//     if(nums[i] < pivot){
+//       left.push(nums[i]);
+//     } else {
+//       right.push(nums[i]);
+//     }
+//   }
+//   //or
+//   let fullySorted = quickSort(left).concat(pivot, quickSort(right));
+//   console.log(fullySorted);
+//   return fullySorted;
+//   // return [...quickSort(left), pivot, ...quickSort(right)];
+// };
+//
+// quickSort(nums);
+
 
 const quickSort = (nums) => {
-  if(nums.length <= 1) return nums;
-  let pivot = nums[nums.length - 1];
-  let left = []; //values smaller than pivot
-  let right = []; //values greater than pivot
-  //account for pivot in the array length (last index => length -1)
+  if(nums.lengh <=1 ) return nums;
+  let pivot = nums[nums.length -1];
+  let left = [];
+  let right = [];
   for(let i = 0; i < nums.length-1; i++){
-
-    if(nums[i] < pivot){
+    if(nums[i]<pivot){
       left.push(nums[i]);
-    } else {
+    } else if(nums[i] > pivot){
       right.push(nums[i]);
     }
   }
-  //or
   let fullySorted = quickSort(left).concat(pivot, quickSort(right));
   console.log(fullySorted);
   return fullySorted;
-  // return [...quickSort(left), pivot, ...quickSort(right)];
 };
-
-quickSort(nums);
